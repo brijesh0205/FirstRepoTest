a0d8cd393c15865c2d44cdc3ca12843e3ab10883
Quikr <quikr@Quikrs-MacBook-Pro-2.local>
2017-01-10 23:43:38 +0530

    first

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Quikr on 10/01/17.
+//  Copyright © 2017 Quikr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by Quikr on 10/01/17.
+//  Copyright © 2017 Quikr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
